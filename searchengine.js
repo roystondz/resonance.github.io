535b45f7a1d3766b13f77315dd1c44c18d50e8ef
RoystonNitte <nnm22cs146@nmamit.in>
2023-10-30 22:50:37 +0530

    Resonance
    Project

---
+++ searchengine.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var input, filter, cards, cardContainer, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("row");
+    cards = cardContainer.getElementsByClassName("person-item");
+    for (i = 0; i < cards.length; i++) {
+      title = cards[i].querySelector(".card-title");
+      if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+        cards[i].style.display = "";
+      } else {
+        cards[i].style.display = "none";
+      }
+    }
+  }
